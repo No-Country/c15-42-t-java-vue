d8e50dba4930ae61567173eae94c95e62c3b199a
AixaHernandezv <aixahernandezv@gmail.com>
2023-12-03 22:08:43 -0300

    avances main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,42 @@
 
 <template>
   <main>
-    <h1>Home</h1>
+    <section class="container-left">
+      <h1 class="container-left__message">¡Encuentra la planta perfecta para tu hogar!</h1>
+      <p class="container-left__subtitle">Decorativas, útiles en la cocina y además curativas.</p>
+    </section>
+    <section class="container-right">
+      <img class="container-right__img" src="/src/assets/img/planta-portada1.png" alt="" srcset="">
+    </section>
   </main>
 </template>
+
+<style scoped>
+main{
+  display: flex;
+  align-items: center;
+  column-gap: 140px;
+  padding: 0 10%;
+  margin-top: 5rem;
+  }
+
+  .container-left {
+    width: 60%;
+  }
+
+  .container-left__message{
+    font-size: 50px;
+  }
+
+  .container-left__subtitle{
+    font-size: 30px;
+  }
+
+  .container-right{
+    width: 40%;
+  }
+ 
+ .container-right__img{
+  width: 100%;
+ }
+</style>
